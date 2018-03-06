3425d8f9a34eea4342dce642088022dcc7d54a10
sophieheseltine <sophie.heseltine@ucdconnect.ie>
2018-03-06 13:53:19 +0000

    main on, off and switch functions

--- LED3/main.py
+++ LED3/main.py
@@ -1,21 +1,45 @@
-import sys
-import click
-click.disable_unicode_literals_warning = True
+import urllib.request
+import re
 
-@click.command()
-@click.option("--input", default=None, help="input URI (file or URL)")
-def main(input=None):
-    """ Console script for LED3 """
-    print("input", input)
-    
-    N, instructions = parseFile(input)
-    
-    ledTester = LEDTester(N)
-    for instruction in instructions:
-        ledTester.apply(instruction)
-        
-    print("Number of occupied lights: ", ledTester.countOccupied())
-    return 0
+#@click.command()
+#@click.option("--input", default=None, help="input URI (file or URL)")
+def countLightsOn(N, a2d):
+    count = 0
+    for i in range(N):
+        for j in range(N):
+            if a2d[i][j] == 1:
+                count += 1
+                
+    return count
 
-if __name__ == "__main__":
-    sys.exit(main())
+# Turn lights that are off (0) to on (1)
+def turnOn(x1, x2, y1, y2, a2d):
+    for i in range(x1, x2):
+        for j in range(y1, y2):
+            a2d[i][j] = 1
+            
+    return
+
+
+# Turn lights off that are on (1) to off (0)
+def turnOff(x1, x2, y1, y2, a2d):
+    for i in range(x1, x2):
+        for j in range(y1, y2):
+            a2d[i][j] = 0
+            
+    return
+
+def switch(x1, x2, y1, y2, a2d):
+    for i in range(x1, x2):
+        for j in range(y1, y2):
+            if a2d[i][j] == 0:
+                a2d[i][j] = 1
+            elif a2d[i][j] == 1:
+                a2d [i][j] = 0
+                
+    return
+
+#print("Number of occupied lights: ", count)
+
+#if __name__ == "__main__":
+#    main()
